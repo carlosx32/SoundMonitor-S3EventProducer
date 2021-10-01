bdbf5bafad9e13dedb46657677b1177ec1b1610b
Carlos David Camacho <carlos0camacho@gmail.com>
2021-09-30 21:14:35 -0500

    Update reporter.tf

--- IAC/reporter.tf
+++ IAC/reporter.tf
@@ -1,5 +1,5 @@
 module "reporter" {
-  source = "git::https://github.com/Carlosx32/SoundMonitor-IAC-Infrastructure-Common.git//lambda_s3_kafka?ref=3.0.13-rc"
+  source = "git::https://github.com/Carlosx32/SoundMonitor-IAC-Infrastructure-Common.git//use-cases/lambda_s3_kafka?ref=3.0.13-rc"
 
   aws_provider_key= var.aws_provider_key
   aws_provider_secret= var.aws_provider_secret
